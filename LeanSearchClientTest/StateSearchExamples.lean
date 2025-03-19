6bdea99317d23e69f89969759942f4166a434e03
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-03-19 15:18:06 +0000

    common `#search` command. README updated

--- LeanSearchClientTest/StateSearchExamples.lean
+++ LeanSearchClientTest/StateSearchExamples.lean
@@ -43,3 +43,43 @@ description: "Lean State Search does not support the specified revision"
 #guard_msgs in
 example : 0 ≤ 1 := by
   #statesearch
+
+/-!
+Tests using `search` with `statesearch` as the backend.
+-/
+set_option statesearch.queries 1 -- set the number of results to 6
+set_option statesearch.revision "v4.16.0" -- set the revision to v4.16.0
+
+
+/-- info: Try these:
+• #check Int.one_pos
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
+example : 0 < 1 := by
+  #search
+  sorry
+
+set_option statesearch.queries 6
+
+/--
+info: From: Nat.zero_lt_one (type: 0 < 1)
+• apply Nat.zero_lt_one
+• have : 0 < 1 := Nat.zero_lt_one
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
+example : 0 < 1 := by
+  #search
+  sorry
+
+set_option statesearch.revision "v4.15.0"
+
+/-- error: error: "Invalid parameter value"
+description: "Lean State Search does not support the specified revision"
+-/
+#guard_msgs in
+example : 0 ≤ 1 := by
+  #search
