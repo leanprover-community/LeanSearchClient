3591c3f664ac3719c4c86e4483e21e228707bfa2
Kim Morrison <477956+kim-em@users.noreply.github.com>
2025-11-24 12:22:06 +1100

    chore: fix tests (#23)

--- LeanSearchClientTest/StateSearchExamples.lean
+++ LeanSearchClientTest/StateSearchExamples.lean
@@ -12,11 +12,12 @@ tactic `#statesearch`.
 -/
 
 set_option statesearch.queries 1 -- set the number of results to 6
-set_option statesearch.revision "v4.16.0" -- set the revision to v4.16.0
+set_option statesearch.revision "v4.22.0" -- set the revision to v4.16.0
 
-
-/-- info: Try these:
-• #check Int.one_pos
+/--
+info: Try these:
+  • #check zero_lt_one -- ∀ {α : Type u_1} [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
 ---
 warning: declaration uses 'sorry'
 -/
@@ -28,9 +29,19 @@ example : 0 < 1 := by
 set_option statesearch.queries 6
 
 /--
-info: From: Nat.zero_lt_one (type: 0 < 1)
-• apply Nat.zero_lt_one
-• have : 0 < 1 := Nat.zero_lt_one
+info: Try these:
+  • #check zero_lt_one -- ∀ {α : Type u_1} [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
+  • #check one_pos -- ∀ {α : Type u_1} [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
+  • #check zero_lt_one' -- ∀ (α : Type u_1) [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
+  • #check ONote.zero_lt_one -- (0 : ONote) < (1 : ONote)
+    ⏎
+  • #check ONote.lt_def -- ∀ {x y : ONote}, x < y ↔ x.repr < y.repr
+    ⏎
+  • #check inv_lt_one_of_one_lt₀ -- ∀ {G₀ : Type u_3} [inst : GroupWithZero G₀] [inst_1 : PartialOrder G₀] [PosMulReflectLT G₀] {a : G₀} [ZeroLEOneClass G₀], (1 : G₀) < a → a⁻¹ < (1 : G₀)
+    ⏎
 ---
 warning: declaration uses 'sorry'
 -/
@@ -52,11 +63,13 @@ example : 0 ≤ 1 := by
 Tests using `search` with `statesearch` as the backend.
 -/
 set_option statesearch.queries 1 -- set the number of results to 6
-set_option statesearch.revision "v4.16.0" -- set the revision to v4.16.0
+set_option statesearch.revision "v4.22.0" -- set the revision to v4.16.0
 
 
-/-- info: Try these:
-• #check Int.one_pos
+/--
+info: Try these:
+  • #check zero_lt_one -- ∀ {α : Type u_1} [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
 ---
 warning: declaration uses 'sorry'
 -/
@@ -68,9 +81,19 @@ example : 0 < 1 := by
 set_option statesearch.queries 6
 
 /--
-info: From: Nat.zero_lt_one (type: 0 < 1)
-• apply Nat.zero_lt_one
-• have : 0 < 1 := Nat.zero_lt_one
+info: Try these:
+  • #check zero_lt_one -- ∀ {α : Type u_1} [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
+  • #check one_pos -- ∀ {α : Type u_1} [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
+  • #check zero_lt_one' -- ∀ (α : Type u_1) [inst : Zero α] [inst_1 : One α] [inst_2 : PartialOrder α] [ZeroLEOneClass α] [NeZero (1 : α)], (0 : α) < (1 : α)
+    ⏎
+  • #check ONote.zero_lt_one -- (0 : ONote) < (1 : ONote)
+    ⏎
+  • #check ONote.lt_def -- ∀ {x y : ONote}, x < y ↔ x.repr < y.repr
+    ⏎
+  • #check inv_lt_one_of_one_lt₀ -- ∀ {G₀ : Type u_3} [inst : GroupWithZero G₀] [inst_1 : PartialOrder G₀] [PosMulReflectLT G₀] {a : G₀} [ZeroLEOneClass G₀], (1 : G₀) < a → a⁻¹ < (1 : G₀)
+    ⏎
 ---
 warning: declaration uses 'sorry'
 -/
